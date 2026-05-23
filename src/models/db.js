776ce36d27d6657e58b30adb2cdabf540dd339be
pius-boy <godwinunimashi.pius@gmail.com>
2026-05-23 13:19:49 +0100

    edited

--- src/models/db.js
+++ src/models/db.js
@@ -12,11 +12,9 @@ import { Pool } from 'pg';
  * postgresql://username:password@host:port/database
  */
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
+    connectionString: process.env.DB_URL,
+    ssl: true
+});
 
 /**
  * Common SSL Issue:
